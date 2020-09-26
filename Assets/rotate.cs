d8809f466bd86ce536ab3c39ef164e28e476af31
brian <brai.lugo@gmail.com>
2020-09-26 11:27:02 -0300

    primer commit

---
+++ Assets/rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.forward);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = transform.position + 0.2f * transform.forward ;
+    }
+}
